5c020f7d105300645845819a51d9bed0f909d9aa
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2015-08-07 17:51:45 +0200

    Adapt el-get boot according to latest doc
    does no longer work though...
    Need time to investigate what i do wrong

--- el-get-pack.el
+++ el-get-pack.el
@@ -4,8 +4,16 @@
 
 ;;; Code:
 
+;; (url-retrieve
+;;  "https://raw.githubusercontent.com/dimitri/el-get/master/el-get-install.el"
+;;  (lambda (s)
+;;    (goto-char (point-max))
+;;    (eval-print-last-sexp)))
+
 (use-package el-get
   :config
+  (add-to-list 'load-path "~/.emacs.d/el-get/el-get")
+  (add-to-list 'el-get-recipe-path "~/.emacs.d/el-get-user/recipes")
   (el-get 'sync))
 
 (provide 'el-get-pack)
