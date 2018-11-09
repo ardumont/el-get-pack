ae8fa7135b27666228fd2100db1d19bf4dbcde2f
Antoine R. Dumont (@ardumont) <antoine.romain.dumont@gmail.com>
2018-11-10 00:04:56 +0100

    el-get-pack: Create bindings to orchestrate el-get-commands

--- el-get-pack.el
+++ el-get-pack.el
@@ -1,21 +1,34 @@
-;;; el-get-pack.el --- Make the packs el-get aware
+;;; el-get-pack.el --- el-get bindings setup
 
 ;;; Commentary:
 
 ;;; Code:
 
-;; (url-retrieve
-;;  "https://raw.githubusercontent.com/dimitri/el-get/master/el-get-install.el"
-;;  (lambda (s)
-;;    (goto-char (point-max))
-;;    (eval-print-last-sexp)))
+(require 'el-get)
 
-(use-package el-get
-  :config
-  (add-to-list 'load-path "~/.emacs.d/el-get/el-get")
-  (add-to-list 'el-get-recipe-path "~/.emacs.d/el-get-user/recipes")
-  ;; (el-get 'sync)
-  )
+(defvar el-get-pack-mode-map
+  (let ((map (make-sparse-keymap)))
+    (define-key map (kbd "C-c l r") 'el-get-remove)
+    (define-key map (kbd "C-c l R") 'el-get-reinstall)
+    (define-key map (kbd "C-c l i") 'el-get-install)
+    (define-key map (kbd "C-c l u") 'el-get-update)
+    (define-key map (kbd "C-c l U") 'el-get-reload)
+    map)
+  "Keymap for el-get-pack mode.")
+
+(define-minor-mode el-get-pack-mode
+  "Minor mode to consolidate el-get-pack extensions.
+
+\\{el-get-pack-mode-map}"
+  :lighter " EGP"
+  :keymap el-get-pack-mode-map
+  :global t)
+
+(define-globalized-minor-mode global-el-get-pack-mode el-get-pack-mode el-get-pack-on)
+
+(defun el-get-pack-on ()
+  "Turn on `el-get-pack-mode'."
+  (el-get-pack-mode +1))
 
 (provide 'el-get-pack)
 ;;; el-get-pack.el ends here
